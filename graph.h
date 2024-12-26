5e27d504a0683bfbd19e8a955cdf816f2255bef2
Wildan <wildannabil101@gmail.com>
2024-12-26 18:09:00 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,44 +1,47 @@
-#ifndef GRAPH_H
-#define GRAPH_H
-
+#ifndef TRANSPORT_H_INCLUDED
+#define TRANSPORT_H_INCLUDED
+#define firstIntersection(G) ((G).firstIntersection)
+#define nextIntersection(I) ((I)->nextIntersection)
+#define firstRoad(I) ((I)->firstRoad)
+#define intersectionID(I) ((I)->intersectionID)
+#define destIntersectionID(R) ((R)->destIntersectionID)
+#define roadLength(R) ((R)->length)
+#define roadSafetyScore(R) ((R)->safetyScore)
+#define nextRoad(R) ((R)->nextRoad)
 #include <iostream>
-#include <vector>
-#include <map>
-#include <limits>
 #include <string>
 
 using namespace std;
 
-const int INF = numeric_limits<int>::max();
+typedef struct intersection *adrIntersection;
+typedef struct road *adrRoad;
 
-class Graph {
-private:
-    map<int, map<int, int>> adjList; // adjacency list: node -> {neighbor, weight}
-    map<int, int> traffic;          // traffic data: node -> traffic level
-
-public:
-    // Add edge to the graph
-    void addEdge(int u, int v, int weight);
-
-    // Set traffic level for a node
-    void setTraffic(int node, int trafficLevel);
-
-    // Get the shortest path using Dijkstra's Algorithm
-    vector<int> shortestPath(int start, int end);
-
-    // Find the busiest node based on traffic data
-    int findBusiestNode();
-
-    // Find alternate safe routes avoiding a specific node
-    vector<int> safeRoute(int start, int end, int blockedNode);
-
-    // Find the path with the lowest total weight (different from shortest path in distance)
-    vector<int> lowestWeightPath(int start, int end);
-
-    // Find all reachable nodes from a given start node
-    vector<int> reachableNodes(int start);
-
+struct intersection {
+    string intersectionID;
+    adrIntersection nextIntersection;
+    adrRoad firstRoad;
+    bool isBlocked;
 };
 
-#endif // GRAPH_H
+struct road {
+    string destIntersectionID;
+    int length;
+    int safetyScore;
+    adrRoad nextRoad;
+};
 
+struct transportNetwork {
+    adrIntersection firstIntersection;
+};
+
+void createIntersection(string newIntersectionID, adrIntersection &I);
+void initNetwork(transportNetwork &N);
+void addIntersection(transportNetwork &N, string newIntersectionID);
+void addRoad(transportNetwork &N, string fromIntersectionID, string toIntersectionID, int length, int safetyScore);
+void printNetwork(transportNetwork &N);
+void updateIntersectionStatus(transportNetwork &N);
+void searchByShortestPath(transportNetwork &N, string startIntersectionID, string targetIntersectionID);
+void searchBySafestPath(transportNetwork &N, string startIntersectionID, string targetIntersectionID);
+void findEmergencyRoute(transportNetwork &N, string startIntersectionID, string targetIntersectionID, string blockedIntersections);
+
+#endif // TRANSPORT_H_INCLUDED
